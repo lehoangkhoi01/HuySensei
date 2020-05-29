4ac2786ee262a427c060d34853f477a215b997bf
unknown <khoi@gmail.com>
2020-05-29 12:48:23 +0700

    first init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// object Creation
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstname = "Le";
+// company.ceo.sureName = "Khoi";
+// company.ceo.age = 40;
+//
+// var stockPropName = "stock of company";
+// company.stockPropName = 110;
+// console.log(company);
+// var facebook = {
+//     name: "Facebook",
+//     ceo: {
+//       firstname: "Le",
+//       sureName: "Khoi",
+//       age: 40,
+//     },
+//     $stock: 110,
+// };
+//
+// console.log(facebook);
